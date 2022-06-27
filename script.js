3a4a03f0e250e8388b23e34f56634b52940f8f06
Abhishek Dasgupta <92089364+abhishekgit03@users.noreply.github.com>
2022-06-27 11:04:38 +0530

    Updated

--- script.js
+++ script.js
@@ -1,40 +1,10 @@
 async function getdata(categ){
+    c=1
     console.log("Get called")
     var url = 'https://gnews.io/api/v4/top-headlines?token=2619b7773b89f1bee4232007bca232e6&lang=en&topic='+categ;
     const res = await fetch(url)
     const data = await res.json() 
     func2(1)
-
-    // -------------------main function---------------------------------------
-    function func2(c)
-    {
-        var title=data['articles'][c]['title']
-        var description=data['articles'][c]['description'].slice(0,100)+"..."
-        var link=data['articles'][c]['url']
-        var urltoimage=data['articles'][c]['image']
-        $('.title').html(title);
-        $('.description').html(description);
-        $('.link').attr('href',link);
-        $('.urltoimage').attr('src',urltoimage)
-    
-        var title1=data['articles'][c-1]['title']
-        var description1=data['articles'][c-1]['description'].slice(0,100)+"..."
-        var link1=data['articles'][c-1]['url']
-        var urltoimage1=data['articles'][c-1]['image']
-        $('.title1').html(title1);
-        $('.description1').html(description1);
-        $('.link1').attr('href',link1);
-        $('.urltoimage1').attr('src',urltoimage1)
-    
-        var title2=data['articles'][c+1]['title']
-        var description2=data['articles'][c+1]['description'].slice(0,100)+"..."
-        var link2=data['articles'][c+1]['url']
-        var urltoimage2=data['articles'][c+1]['image']
-        $('.title2').html(title2);
-        $('.description2').html(description2);
-        $('.link2').attr('href',link2);
-        $('.urltoimage2').attr('src',urltoimage2)
-    };
 
      //---------------nextbutton------------------
 
@@ -42,7 +12,7 @@ async function getdata(categ){
     button.addEventListener("click", () => {
         length_array=data['articles'].length
         c=c+1
-        if(c>=length_array)                    //testing-complete-working fine
+        if(c>=length_array)                 //testing-complete-working fine
         {
             c=0
         }  
@@ -51,23 +21,55 @@ async function getdata(categ){
         console.log("Clicked")                
         console.log(data['articles'][c])        
     });
-
     //---------------backbutton------------------
     var button=document.querySelector(".back");
     button.addEventListener("click", () => {
         c=c-1
         console.log("Clicked")
         console.log(data['articles'][c])
-        if(c<=0)                              //testing-complete-working fine
+        if(c<=0)                            //testing-complete-working fine
         {
             c=length_array
         }  
         func2(c);  
     });
-   
+
+
+    function func2(c)                        //main-function
+    {
+    var title=data['articles'][c]['title']
+    var description=data['articles'][c]['description'].slice(0,100)+"..."
+    var link=data['articles'][c]['url']
+    var urltoimage=data['articles'][c]['image']
+    $('.title').html(title);
+    $('.description').html(description);
+    $('.link').attr('href',link);
+    $('.urltoimage').attr('src',urltoimage)
+
+    var title1=data['articles'][c-1]['title']
+    var description1=data['articles'][c-1]['description'].slice(0,100)+"..."
+    var link1=data['articles'][c-1]['url']
+    var urltoimage1=data['articles'][c-1]['image']
+    $('.title1').html(title1);
+    $('.description1').html(description1);
+    $('.link1').attr('href',link1);
+    $('.urltoimage1').attr('src',urltoimage1)
+
+    var title2=data['articles'][c+1]['title']
+    var description2=data['articles'][c+1]['description'].slice(0,100)+"..."
+    var link2=data['articles'][c+1]['url']
+    var urltoimage2=data['articles'][c+1]['image']
+    $('.title2').html(title2);
+    $('.description2').html(description2);
+    $('.link2').attr('href',link2);
+    $('.urltoimage2').attr('src',urltoimage2)
+    };
+
+
 }       
 
-getdata('general')   // default category
+getdata('general')    //default category
+
 
 var business=document.querySelector(".business");
 business.addEventListener("click", () => {
@@ -89,8 +91,8 @@ sports.addEventListener("click", () => {
     getdata('sports')
 });
 
-function myFunction()   //Dark Mode
- {
+function myFunction()   //dark mode
+{
     var element = document.getElementById('hero');
     var headline=document.getElementById('headline');
     var navtoggle=document.getElementById('nav');
